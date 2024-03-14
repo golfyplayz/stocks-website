a918b07c61dc4d71c9748e656013e754e38be35f
golfyplayz <jackson.verdoorn@gmail.com>
2024-03-14 14:34:09 -0500

    added cors

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app,use(cors());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
@@ -8,4 +10,9 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
     console.log('Hello World');
     res.send('Hello World');
+});
+
+app.get('/test', (req, res) => {
+    console.log('Hello World but theres a test in the url');
+    res.send('Hello World but theres a test in the url');
 });
