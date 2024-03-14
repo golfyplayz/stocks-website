7e4cedbb89473a2c931d2abddb611d335972463d
golfyplayz <jackson.verdoorn@gmail.com>
2024-03-14 14:44:53 -0500

    remove comma

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app,use(cors());
+app.use(cors());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
