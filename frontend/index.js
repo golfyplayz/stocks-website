904a290311a65814146eabbf0a170100ab903c47
golfyplayz <jackson.verdoorn@gmail.com>
2024-04-30 14:28:19 -0500

    refactor folders

--- frontend/index.js
+++
@@ -1,5 +0,0 @@
-const react = require("react");
-const reactDom = require("react-dom");
-const container = document.getElementById("root");
-const root = reactDom.createRoot(container);
-root.render(<h1>Hello World</h1>);
