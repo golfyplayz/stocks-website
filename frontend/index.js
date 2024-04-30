a0f91745582248c1686468f6fa784a729b62a084
golfyplayz <jackson.verdoorn@gmail.com>
2024-04-30 13:58:09 -0500

    baisc react

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,5 @@
 const react = require("react");
+const reactDom = require("react-dom");
+const container = document.getElementById("root");
+const root = reactDom.createRoot(container);
+root.render(<h1>Hello World</h1>);
