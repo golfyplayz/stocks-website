4f40fe0b98fc9efb1fd5850dfb3eb53d281f4de3
golfyplayz <jackson.verdoorn@gmail.com>
2024-05-01 08:36:20 -0500

    more stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div className="Calculator">
+      <form>
+          <label for="stockInt" class="label" id="stock">Last Week's Price</label>
+          <input type="number" name="stock" id="stockInt" placeholder="Last Week's Price" class="number"></input>
+          <br></br>
+          <label for="stockFin" class="label" id="stock">This Week's Price</label>
+          <input type="number" name="stock" id="stockFin" placeholder="This Week's Price" class="number"></input>
+          <br></br>
+          <label for="stockAmt" class="label" id="stock">Number of Stocks</label>
+          <input type="number" name="stock" id="stockAmt" placeholder="Number of Stocks" class="number"></input>
+          <br></br>
+          <input type="submit" value="Calculate" id="submit"></input>
+      </form>
     </div>
   );
 }
