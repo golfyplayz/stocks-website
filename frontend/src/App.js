008853e741ccfb9587d75ecdb261f014f8536e6c
golfyplayz <jackson.verdoorn@gmail.com>
2024-05-02 12:20:13 -0500

    get basic renders and add output function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="Calculator">
+    <div className="form">
       <form>
           <label for="stockInt" class="label" id="stock">Last Week's Price</label>
           <input type="number" name="stock" id="stockInt" placeholder="Last Week's Price" class="number"></input>
@@ -17,6 +17,27 @@ function App() {
       </form>
     </div>
   );
-}
+};
 
-export default App;
+function output(amount) {
+  if (amount > 0) {
+    return (
+      <div className="output">
+        <h1>Money Made:</h1>
+        <br></br>
+        <h3>${amount}</h3>
+      </div>
+    )
+  } else {
+    return (
+      <div className="output">
+        <h1>Money Lost:</h1>
+        <br></br>
+        <h3>-${amount}</h3>
+      </div>
+    )
+  }
+};
+
+const calcFuncs = { App, output };
+export { calcFuncs };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { calcFuncs } from './App';
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', Calculate);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <calcFuncs.App />
   </React.StrictMode>
 );
+
+function Calculate() {
+  let stockInt = document.getElementById('stockInt').value;
+  let stockFin = document.getElementById('stockFin').value;
+  let stockAmt = document.getElementById('stockAmt').value;
+  let amount = (stockFin - stockInt) * stockAmt;
+  root.render(
+    <React.StrictMode>
+      <calcFuncs.App />
+      <calcFuncs.output amount={amount} />
+    </React.StrictMode>
+  )
+}
