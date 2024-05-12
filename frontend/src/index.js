70eeadc72e20dc0fe4da32d323a447dd3b112ed3
golfyplayz <jackson.verdoorn@gmail.com>
2024-05-12 08:09:44 -0500

    working on smaller screen render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 function App() {
   const [width, setWidth] = useState(window.innerWidth);
@@ -13,24 +14,36 @@ function App() {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
-  if (width < 600) {
-    return <h1>Too Small</h1>;
-  } else {
+  if (width < 800) {
+    return (
+      <div id='main'>
+        <nav>
+          <ul className="nav">
+            <li className="links" id="home"><a href="./"><i className="fa-solid fa-house fa-lg"></i></a></li>
+          </ul>
+          <h2 className='user'>Welcome User!</h2>
+          <ul className="menu">
+            <li className="links" id="leader"><a href="./"><i class="fa-solid fa-bars fa-lg"></i></a></li>
+          </ul>
+        </nav>
+      </div>
+  );} else {
     return (
     <div id='main'>
       <nav>
       <ul className="nav">
-        <li className="links" id="home"><a href=""><i className="fa-solid fa-house fa-lg"></i></a></li>
-        <li className="links" id="leader"><a href="">Leaderboard</a></li>
-        <li className="links" id="calc"><a href="">Calculator</a></li>
-        <li className="links" id="lookup"><a href="">Look Up</a></li>
+        <li className="links" id="home"><a href="./"><i className="fa-solid fa-house fa-lg"></i></a></li>
+        <li className="links" id="leader"><a href="./">Leaderboard</a></li>
+        <li className="links" id="calc"><a href="./">Calculator</a></li>
+        <li className="links" id="lookup"><a href="./">Stocks</a></li>
       </ul>
       <ul className="acct">
-        <li className="links" id="acct"><a href=""><i className="fa-solid fa-user fa-lg"></i></a></li>
+        <li className="links" id="acct"><a href="./"><i className="fa-solid fa-user fa-lg"></i></a></li>
       </ul>
     </nav>
     <div className="body">
-      <div className="other"></div>
+      
+      <div className="stocks"></div>
       <div className="info"></div>
       <div className="leader"></div>
     </div>
