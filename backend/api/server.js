988c84ac90fdcffa8d2c1da6d2c39006e19f2a31
golfyplayz <jackson.verdoorn@gmail.com>
2024-07-05 13:30:47 -0500

    fix port issues

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const { createTables } = require('./db'); // Ensure you're importing createTables correctly
+
 const app = express();
 app.use(cors());
 
-
-
+// Listen on the port provided by the environment or fallback to 3000
+const port = process.env.PORT || 3000;
 
 createTables().then(() => {
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
     });
-  });
+});
