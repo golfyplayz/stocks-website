f5df48e5295fb9b61da6e6d27d891b53cbaa8275
golfyplayz <jackson.verdoorn@gmail.com>
2024-08-23 19:35:58 -0500

    stuff

--- backend/api/server.js
+++ backend/api/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Listen on the port provided by the environment or fallback to 3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3050;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,18 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home.js';
 import Leader from './pages/leader.js';
 import Calc from './pages/calc.js';
-
+import Login from './pages/login.js';
 
 function App() {
-    return(
+    return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/leader" element={<Leader/>}/>
-                <Route path="/calc" element={<Calc/>}/>
+                <Route path="/" element={<Home />} />
+                <Route path="/leader" element={<Leader />} />
+                <Route path="/calc" element={<Calc />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </Router>
-    )
+    );
 }
 
 export default App;
