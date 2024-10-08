394e1b508d2180242a70663a21615b0ce71851d7
golfyplayz <jackson.verdoorn@gmail.com>
2024-10-07 21:07:48 -0500

    add datebase storage for storing calculations

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,48 +1,47 @@
 const express = require('express');
 const cors = require('cors');
+const { Pool } = require('pg');
 const app = express();
 app.use(cors());
+app.use(express.json()); // Middleware to parse JSON bodies
 
-let data = [
-    {
-        "name": "Mason",
-        "age": 10,
-        "city": "Kansas City",
+const pool = new Pool({
+    connectionString: 'postgresql://admin:8DsKX3emwJrLwfZjeLG6eISFJqku4K3n@dpg-cs27c0u8ii6s739d1ltg-a.ohio-postgres.render.com/stocksdatabase',
+    ssl: {
+        rejectUnauthorized: false,
     },
-    {
-        "name": "Liam",
-        "age": 11,
-        "city": "New York",
-    },
-    {
-        "name": "Emma",
-        "age": 12,
-        "city": "Los Angeles",
-    },
-    {
-        "name": "Olivia",
-        "age": 13,
-        "city": "Chicago",
-    }
-]
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
 });
 
-app.get('/', (req, res) => {
-    console.log('get main data');
-    res.send(data);
+app.listen(8383, () => {
+    console.log('Server is running on port 8383');
 });
 
-app.get('/test/:id', (req, res) => {
-    console.log('get test data');
-    const id = req.params.id
-    console.log('id:', id);
-    if(id > 0 && id <= data.length) {
-        const name = data[id - 1].name;
-        res.send(name);
-    } else {
-        res.status(404).send('Not found');
+app.delete('/purge', async (req, res) => {
+    let dateNow = Date.now();
+    let purgeDate = dateNow - 2629743;
+    try {
+        const queryText = 'DELETE FROM calculations WHERE date < $1';
+        await pool.query(queryText, [purgeDate]);
+        res.status(200).json({ message: 'Old data purged' });
+    } catch (err) {
+        console.error('Error purging old data:', err);
+        res.status(500).json({ message: 'Internal Server Error' });
     }
-}); 
+});
+
+
+
+app.post('/data', async (req, res) => {
+    const { init, fin, amnt, name, total } = req.body;
+    console.log('Received data:', { init, fin, amnt, name, total });
+    let date = Date.now();
+
+    try {
+        const queryText = 'INSERT INTO calculations (initial_value, final_value, number_stocks, name, calculation, date) VALUES ($1, $2, $3, $4, $5, $6)';
+        await pool.query(queryText, [init, fin, amnt, name, total, date]);
+        res.status(201).json({ message: 'Data received and inserted into the database' });
+    } catch (err) {
+        console.error('Error inserting data into the database:', err);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
